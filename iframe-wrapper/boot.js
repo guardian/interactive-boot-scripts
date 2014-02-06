29bdbea013b4b0a763cbdd6f37ddae321d72834f
andymason <andrew.mason@guardian.co.uk>
2014-02-06 17:33:08 +0000

    Restrict messages to interactive.guim.co.uk

--- iframe-wrapper/boot.js
+++ iframe-wrapper/boot.js
@@ -12,6 +12,10 @@ define([], function () {
 
                 // Listen for requests from the window
                 window.addEventListener('message', function(event) {
+                    if (event.origin !== 'http://interactive.guim.co.uk') {
+                        return;
+                    }
+
                     var message = JSON.parse(event.data); // IE 8 + 9 only support strings
 
                     if (message.type === 'set-height') {
